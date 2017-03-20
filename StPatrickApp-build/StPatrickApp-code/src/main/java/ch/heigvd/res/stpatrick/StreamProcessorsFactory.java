d1f17367d8db726ee1d1795ec3c7b1eba5a39300
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-03-20 12:39:58 +0100

    commit final

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
@@ -1,11 +1,14 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.Reader;
+import java.io.Writer;
+
 /**
  * This class is responsible for providing different types of Stream Processors.
  * 
  * @author Olivier Liechti
  */
-public class StreamProcessorsFactory implements IStreamProcessorsFactory {
+public class StreamProcessorsFactory implements IStreamProcessorsFactory, IStreamDecoratorController {
 
   @Override
   public IStreamProcessor getProcessor() {
@@ -19,5 +22,15 @@ public class StreamProcessorsFactory implements IStreamProcessorsFactory {
     }
     throw new UnknownNameException("The factory does not know any processor called " + processorName);
   }
+
+   @Override
+   public Reader decorateReader(Reader inputReader) {
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
+
+   @Override
+   public Writer decorateWriter(Writer outputWriter) {
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
 
 }
